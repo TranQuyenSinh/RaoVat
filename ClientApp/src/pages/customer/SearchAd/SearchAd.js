a645e56d90da1f1d6d7d79b520c2fe8618efe743
tranquyensinh <vn.quyensinh@gmail.com>
2023-10-29 18:54:48 +0700

    Tạo component FilterBar

--- ClientApp/src/pages/customer/SearchAd/SearchAd.js
+++ ClientApp/src/pages/customer/SearchAd/SearchAd.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
 import './SearchAd.scss'
+import FilterBar from './FilterBar'
 
 const SearchAd = () => {
     const [searchParams, setSearchParams] = useSearchParams()
     const keyword = searchParams.get('q') || ''
+
+    const handleFilter = filter => {
+        console.log(filter)
+    }
     return (
         <>
+            <FilterBar onSubmit={handleFilter} />
             <div>Search key: {keyword}</div>
         </>
     )
---
+++ ClientApp/src/pages/customer/SearchAd/SelectOrderModal.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import { Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap'
+import './SelectOrderModal.scss'
+import RangeSlider from 'react-range-slider-input'
+import 'react-range-slider-input/dist/style.css'
+import { formatNumber } from '../../../utils'
+import CustomRadio from '../../../components/input/CustomRadio/CustomRadio'
+
+const SelectOrderModal = ({ isOpen, toggle, onSubmit }) => {
+    const [value, setValue] = useState()
+
+    const handleSubmit = () => {
+        onSubmit(value)
+        toggle()
+    }
+
+    const orderData = [
+        { title: 'Tin mới trước', value: 'createdAt', icon: 'fa-regular fa-clock' },
+        { title: 'Giá thấp trước', value: 'price', icon: 'fa-solid fa-dollar-sign' },
+    ]
+
+    return (
+        <Modal isOpen={isOpen} toggle={toggle} className='order-modal'>
+            <ModalHeader>
+                <strong>Sắp xếp theo</strong>
+                <span>
+                    <button onClick={toggle} className='btn btn-close'></button>
+                </span>
+            </ModalHeader>
+            <ModalBody>
+                {orderData?.map((item, index) => (
+                    <CustomRadio key={index} onClick={() => setValue({ ...item })} name='order'>
+                        <i className={`${item.icon} me-2`}></i>
+                        {item.title}
+                    </CustomRadio>
+                ))}
+            </ModalBody>
+            <ModalFooter className='fill'>
+                <div onClick={handleSubmit} className='btn btn-main text-uppercase'>
+                    Áp dụng
+                </div>
+            </ModalFooter>
+        </Modal>
+    )
+}
+
+export default SelectOrderModal

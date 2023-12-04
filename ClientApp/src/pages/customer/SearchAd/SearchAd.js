af01ee756a59423124932252be606979a851059e
tranquyensinh <vn.quyensinh@gmail.com>
2023-12-04 16:05:17 +0700

    Fix bug pagination trong SearchAd

--- ClientApp/src/pages/customer/SearchAd/SearchAd.js
+++ ClientApp/src/pages/customer/SearchAd/SearchAd.js
@@ -6,7 +6,7 @@ import Section from '../../../components/customer/Section/Section'
 import { formatNumber, moment } from '../../../utils'
 import { searchAd } from '../../../services'
 import { useSelector } from 'react-redux'
-import Pagination from '../../../components/pagination/Pagination'
+import CustomPaginate from '@components/pagination/CustomPaginate'
 import { saveAdToFavorite } from '../../../services'
 import { toast } from 'react-toastify'
 import SearchNotFound from '../../../components/notfound/AdNotFound/SearchNotFound'
@@ -18,8 +18,8 @@ const SearchAd = () => {
     const [filter, setFilter] = useState()
     const { currentLocation } = useSelector(state => state.app)
     const { isLoggedIn, currentUser } = useSelector(state => state.user)
-    const [currentPage, setCurrentPage] = useState(1)
-    const [totalCount, setTotalCount] = useState(0)
+    const [currentPage, setCurrentPage] = useState(0)
+    const [totalItems, setTotalItems] = useState(0)
     const navigate = useNavigate()
 
     const [ads, setAds] = useState([])
@@ -34,8 +34,8 @@ const SearchAd = () => {
         }
         let {
             data: { data, totalCount },
-        } = await searchAd(search, currentPage - 1, currentUser?.id)
-        setTotalCount(totalCount)
+        } = await searchAd(search, currentPage, currentUser?.id)
+        setTotalItems(totalCount)
         setAds(data)
     }
 
@@ -62,11 +62,12 @@ const SearchAd = () => {
     }
 
     useEffect(() => {
-        setCurrentPage(1)
-    }, [filter])
+        setCurrentPage(0)
+    }, [filter, keyword])
 
     useEffect(() => {
         searchProduct()
+        window.scrollTo(0, 0)
     }, [filter, currentLocation, currentPage, keyword])
 
     useEffect(() => {
@@ -76,57 +77,60 @@ const SearchAd = () => {
     return (
         <>
             <FilterBar onSubmit={handleFilter} />
-            <Pagination
-                className='pagination'
-                currentPage={currentPage}
-                totalCount={totalCount}
-                pageSize={10}
-                siblingCount={2}
-                onPageChange={page => setCurrentPage(page)}
-            />
-            <div className='search-container'>
-                <div className='mt-2 mb-3 fw-bold'>Tìm kiếm với từ khóa: {keyword}</div>
-                <Section className='search-list-container'>
-                    {ads?.length > 0 &&
-                        ads?.map(item => (
-                            <div
-                                key={item.id}
-                                className='product-item'
-                                onClick={() => navigate('/tin-dang/' + item.id)}>
-                                <div className='product-image-wrapper'>
-                                    <img src={item.thumbnail} />
-                                    {item.status ? (
-                                        <CustomBadge type='success'>Mới</CustomBadge>
-                                    ) : (
-                                        <CustomBadge type='warning'>Đã sử dụng</CustomBadge>
-                                    )}
-                                </div>
-                                <div className='product-info'>
-                                    <div className='top'>
-                                        <div className='title'>
-                                            <span>{item.title}</span>
+            {ads?.length > 0 && (
+                <>
+                    <div className='search-container'>
+                        <div className='mt-2 mb-3 fw-bold'>Tìm kiếm với từ khóa: {keyword}</div>
+                        <Section className='search-list-container'>
+                            {ads?.length > 0 &&
+                                ads?.map(item => (
+                                    <div
+                                        key={item.id}
+                                        className='product-item'
+                                        onClick={() => navigate('/tin-dang/' + item.id)}>
+                                        <div className='product-image-wrapper'>
+                                            <img src={item.thumbnail} />
+                                            {item.status ? (
+                                                <CustomBadge type='success'>Mới</CustomBadge>
+                                            ) : (
+                                                <CustomBadge type='warning'>Đã sử dụng</CustomBadge>
+                                            )}
                                         </div>
-                                        <div className='price'>{formatNumber(item.price)}</div>
-                                    </div>
-                                    <div className='bottom'>
-                                        <div className='location'>
-                                            <i className='fa-regular fa-clock me-2'></i>
-                                            {moment(item.createdAt).fromNow()} . {item.district} - {item.province}
+                                        <div className='product-info'>
+                                            <div className='top'>
+                                                <div className='title'>
+                                                    <span>{item.title}</span>
+                                                </div>
+                                                <div className='price'>{formatNumber(item.price)}</div>
+                                            </div>
+                                            <div className='bottom'>
+                                                <div className='location'>
+                                                    <i className='fa-regular fa-clock me-2'></i>
+                                                    {moment(item.createdAt).fromNow()} . {item.district} -{' '}
+                                                    {item.province}
+                                                </div>
+                                                <i
+                                                    onClick={e => handleSaveAd(e, item, !item.isFavorite)}
+                                                    className={
+                                                        item.isFavorite
+                                                            ? 'fa-solid fa-heart favorite-icon'
+                                                            : 'fa-regular fa-heart favorite-icon'
+                                                    }></i>
+                                            </div>
                                         </div>
-                                        <i
-                                            onClick={e => handleSaveAd(e, item, !item.isFavorite)}
-                                            className={
-                                                item.isFavorite
-                                                    ? 'fa-solid fa-heart favorite-icon'
-                                                    : 'fa-regular fa-heart favorite-icon'
-                                            }></i>
                                     </div>
-                                </div>
-                            </div>
-                        ))}
-                    {ads?.length == 0 && <SearchNotFound className={'py-5'} />}
-                </Section>
-            </div>
+                                ))}
+                            {ads?.length == 0 && <SearchNotFound className={'py-5'} />}
+                        </Section>
+
+                        <CustomPaginate
+                            totalItems={totalItems}
+                            itemsPerPage={10}
+                            onChange={pageNum => setCurrentPage(pageNum)}
+                        />
+                    </div>
+                </>
+            )}
         </>
     )
 }
